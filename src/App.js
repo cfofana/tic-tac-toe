ef8cd3c1a2ebb59e2e544cff8455eacf83756033
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-09-04 21:28:46 -0400

    Add rows of buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 export default function Square(){
   return(
     <div>
-      <button className="square">X</button>
+      <div class="row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div class="row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div class="row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
     </div>
   );
 }
