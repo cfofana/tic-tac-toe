815a4739cfb581fefe8e5e79e3414031a021d7c7
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-09-05 21:18:48 -0400

    Add square component and pass its value as prop

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-export default function Square(){
+function Square({ value }){
+  return <button className="square">{value}</button>;
+  
+}
+
+export default function Board(){
   return(
     <div>
-      <div class="row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <div class="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
-      <div class="row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <div class="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
-      <div class="row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <div class="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </div>
   );
